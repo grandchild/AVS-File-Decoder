2cebdbcaa4f98dbdb28edc01143b19fb775ece29
jakob <jakobk@freitagsrunde.org>
2013-10-01 02:58:13 +0200

    Added parsing of APE/DLL components. Added AVS Trans component. Added Buffer Save component. Added Set Render Mode component. Added empty decoders for a few other components. Fixed range bug in Effect List parsing.

--- js/convert.js
+++ js/convert.js
@@ -1,15 +1,33 @@
 var sizeInt = 4;
 var presetHeaderLength = 25;
+var builtinMax = 16384;
 
 var componentTable =
 		[
-			// code, name, regexp
-			{'name': "Effect List",    'code': 0xfffffffe, 'group': "",
-				'func': "effectList"},
-			{'name': "Color Modifier", 'code': 0x2D, 'group': "Trans",
-				'func': "colorModifier"},
-			{'name': "Comment",        'code': 0x15, 'group': "Misc",
-				'func': "comment"},
+		//// built-in components
+			{'name': "Effect List",
+				'code': 0xfffffffe, 'group': "", 'func': "effectList"},
+			{'name': "Buffer Save",
+				'code': 0x12, 'group': "Misc", 'func': "bufferSave"},
+			{'name': "Comment",
+				'code': 0x15, 'group': "Misc", 'func': "comment"},
+			{'name': "Set Render Mode",
+				'code': 0x28, 'group': "Misc", 'func': "renderMode"},
+			{'name': "Color Modifier",
+				'code': 0x2D, 'group': "Trans", 'func': "colorModifier"},
+		//// APEs
+			{'name': "AVS Trans Automation",
+				'code': // Misc: AVSTrans Automation.......
+					[0x4D, 0x69, 0x73, 0x63, 0x3A, 0x20, 0x41, 0x56, 0x53, 0x54, 0x72, 0x61, 0x6E, 0x73, 0x20, 0x41, 0x75, 0x74, 0x6F, 0x6D, 0x61, 0x74, 0x69, 0x6F, 0x6E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+				'group': "Misc", 'func': "avsTrans"},
+			{'name': "Texer II",
+				'code': // Acko.net: Texer II..............
+					[0x41, 0x63, 0x6B, 0x6F, 0x2E, 0x6E, 0x65, 0x74, 0x3A, 0x20, 0x54, 0x65, 0x78, 0x65, 0x72, 0x20, 0x49, 0x49, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+				'group': "Render", 'func': "texer2"},
+			{'name': "Color Map",
+				'code': // Color Map.......................
+					[0x43, 0x6F, 0x6C, 0x6F, 0x72, 0x20, 0x4D, 0x61, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+				'group': "Trans", 'func': "colorMap"},
 		];
 
 function convertPreset (preset) {
@@ -19,7 +37,7 @@ function convertPreset (preset) {
 	try {
 		var clearFrame = decodePresetHeader(blob8.subarray(0, presetHeaderLength));
 		json.push(jsonKeyValBool('clearFrame', clearFrame));
-		var components = convertComponents(blob8.subarray(presetHeaderLength));
+		var components = convertComponents(blob8.subarray(presetHeaderLength), 0);
 		json.push(jsonKeyArr('components', components));
 	} catch (e) {
 		if(e instanceof ConvertException) {
@@ -32,22 +50,24 @@ function convertPreset (preset) {
 	return cJoin(json);
 }
 
-function convertComponents (blob) {
+function convertComponents (blob, level) {
 	var fp = 0;
 	var json = [];
 	while(fp < blob.length) {
-		var i = getComponentIndex(blob, fp);
-		var s = getComponentSize(blob, fp+sizeInt);
+		var code = getUInt32(blob, fp);
+		var i = getComponentIndex(code, blob, fp);
+		var isDll = code>builtinMax && code!==0xfffffffe;
+		var size = getComponentSize(blob, fp+sizeInt+isDll*32);
 		if(i<0) {
 			var res = jsonKeyVal('type','Unknown: ('+(-i)+')');
 		} else {
-			var res = window["decode_"+componentTable[i].func](blob, fp+sizeInt*2);
+			var res = window["decode_"+componentTable[i].func](blob, fp+sizeInt*2+isDll*32);
 		}
 		if(!res || typeof res !== "string") { // should not happen, decode functions should throw their own.
 			throw new ConvertException("Unknown convert error");
 		}
 		json.push(jsonKeyObj(null, res));
-		fp += s + sizeInt*2;
+		fp += size + sizeInt*2 + isDll*32;
 	}
 	if(pedanticMode && blob[fp-1]!==0x00) {
 		// no trailing zero in AVS > 2.? (2.8?)
@@ -56,17 +76,25 @@ function convertComponents (blob) {
 	return cJoin(json);
 }
 
-function getComponentIndex (blob, offset) {
-	var code = getUInt32(blob, offset);
-	for (var i = 0; i < componentTable.length; i++) {
-		if(code === componentTable[i].code) {
-			log("Found component: "+componentTable[i].name);
-			return i;
+function getComponentIndex (code, blob, offset) {
+	if(code<builtinMax || code===0xfffffffe) {
+		for (var i = 0; i < componentTable.length; i++) {
+			if(code === componentTable[i].code) {
+				log("Found component: "+componentTable[i].name+" ("+code+")");
+				return i;
+			}
 		}
-	};
+	} else {
+		for (var i = 0; i < componentTable.length; i++) {
+			if(componentTable[i].code instanceof Array &&
+					cmpBytes(blob, offset+sizeInt, componentTable[i].code)) {
+				log("Found component: "+componentTable[i].name);
+				return i;
+			}
+		}
+	}
 	log("Found unknown component (code:"+code+")");
 	return -code;
-	//throw new ConvertException("Unknown component type: '"+code+"'");
 }
 
 function getComponentSize (blob, offset) {
@@ -129,8 +157,18 @@ function decode_effectList (blob, offset) {
 		codeJson.push(jsonKeyVal('frame', getString(blob, extOffset+sizeInt*4+initSize, frameSize)));
 		json.push(jsonKeyObj('code', cJoin(codeJson)));
 	} //else: old Effect List format, inside components just start
-	var content = convertComponents(blob.subarray(contOffset, contSize));
+	var content = convertComponents(blob.subarray(contOffset, contOffset+contSize), 1);
 	json.push(jsonKeyArr('components', content));
+	return cJoin(json);
+}
+
+function decode_bufferSave (blob, offset) {
+	var json = [];
+	json.push(jsonKeyVal('type', 'BufferSave'));
+	json.push(jsonKeyVal('mode', getBufferMode(blob[offset])));
+	json.push(jsonKeyVal('buffer', blob[offset+sizeInt]));
+	json.push(jsonKeyVal('blend', getBlendmodeBuffer(blob[offset+sizeInt*2])));
+	json.push(jsonKeyVal('adjustBlend', blob[offset+sizeInt*3]));
 	return cJoin(json);
 }
 
@@ -150,12 +188,46 @@ function decode_colorModifier (blob, offset) {
 	return cJoin(json);
 }
 
+function decode_renderMode (blob, offset) {
+	var json = [];
+	json.push(jsonKeyVal('type', 'SetRenderMode'));
+	json.push(jsonKeyVal('enabled', blob[offset+3]>>7));
+	json.push(jsonKeyVal('blend', getBlendmodeRender(blob[offset])));
+	json.push(jsonKeyVal('adjustBlend', blob[offset+1]));
+	json.push(jsonKeyVal('lineSize', blob[offset+2]));
+	return cJoin(json);
+}
+
+function decode_avsTrans (blob, offset) {
+	var size = getUInt32(blob, offset-sizeInt);
+	var json = [];
+	json.push(jsonKeyVal('type', 'AVS Trans Automation'));
+	json.push(jsonKeyValBool('enabled', getUInt32(blob, offset)===1));
+	json.push(jsonKeyValBool('logging', getUInt32(blob, offset+sizeInt)===1));
+	json.push(jsonKeyValBool('translateFirstLevel', getUInt32(blob, offset+sizeInt*2)===1));
+	json.push(jsonKeyValBool('readCommentCodes', getUInt32(blob, offset+sizeInt*3)===1));
+	json.push(jsonKeyVal('code', getString(blob, offset+sizeInt*4, size-sizeInt*4)));
+	return cJoin(json);
+}
+
+function decode_texer2 (blob, offset) {
+	var json = [];
+	json.push(jsonKeyVal('type', 'Texer2'));
+	return cJoin(json);
+}
+
+function decode_colorMap (blob, offset) {
+	var json = [];
+	json.push(jsonKeyVal('type', 'ColorMap'));
+	return cJoin(json);
+}
+
 /**
  * blank decode function
 
 function decode_ (blob, offset) {
 	var json = [];
-	json.push(jsonKeyVal('type',''));
+	json.push(jsonKeyVal('type', ''));
 	return cJoin(json);
 }
 
@@ -175,7 +247,7 @@ function decodeCodePFBI (blob, offset) {
 	return cJoin(json);
 }
 
-// Blendmodes
+// Blend modes
 function getBlendmodeIn (code) {
 	var blendmodes = {
 			 '0': 'Ignore',
@@ -191,7 +263,7 @@ function getBlendmodeIn (code) {
 			'10': 'Adjustable',
 			'11': 'Multiply',
 			'12': 'Buffer',
-	};
+		};
 	return blendmodes[code];
 }
 
@@ -211,21 +283,25 @@ function getBlendmodeOut (code) {
 			'11': 'Adjustable',
 			// don't ask me....
 			'13': 'Buffer',
-	};
+		};
 	return blendmodes[code];
 }
 
 function getBlendmodeBuffer (code) {
 	var blendmodes = {
-			'0': 'Replace',
-			'1': '50/50',
-			'2': 'Additive',
-			'3': 'EveryOtherPixel',
-			'4': 'Dest-Src',
-			'5': 'EveryOtherLine',
-			'6': 'XOR',
-			'7': 'Maximum',
-	};
+			 '0': 'Replace',
+			 '1': '50/50',
+			 '2': 'Additive',
+			 '5': 'EveryOtherPixel',
+			 '4': 'Dest-Src',
+			 '5': 'EveryOtherLine',
+			 '6': 'XOR',
+			 '7': 'Maximum',
+			 '8': 'Minimum',
+			 '9': 'Src-Dest',
+			'10': 'Multiply',
+			'11': 'Adjustable',
+		};
 	return blendmodes[code];
 }
 
@@ -240,6 +316,17 @@ function getBlendmodeRender (code) {
 			'6': 'Multiply',
 			'7': 'Adjustable',
 			'8': 'XOR',
-	};
+		};
 	return blendmodes[code];
+}
+
+// Buffer modes
+function getBufferMode (code) {
+	var buffermodes = {
+			'0': 'Save',
+			'1': 'Restore',
+			'2': 'AlternateSaveRestore',
+			'3': 'AlternateRestoreSave',
+		};
+	return buffermodes[code];
 }
