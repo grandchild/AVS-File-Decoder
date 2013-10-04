23d191ac60581dcf423d830cc6687e4b3eb56994
idleberg <jan@idleberg.com>
2013-10-04 15:39:40 +0200

    added build tools, re-added bower support and jquery fallback

---
+++ build.bat
@@ -0,0 +1,2 @@
+type js/components-builtin.js js/components-dll.js js/convert.js js/files.js js/main.js js/tables.js js/util.js > js/_combined.js
+java -jar bin/yuicompressor.jar js/_combined.js -o js/_combined.js
