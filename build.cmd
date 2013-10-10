ab529862037b799bb3b00e82942723d1fd0d26d3
idleberg <jan@idleberg.com>
2013-10-10 17:19:37 +0200

    Update files

--- build.cmd
+++
@@ -1,2 +0,0 @@
-type js/components-builtin.js js/components-dll.js js/convert.js js/files.js js/main.js js/util.js js/tables.js js/ui.js > js/_combined.js
-java -jar bin/yuicompressor.jar js/_combined.js -o js/_combined.js
