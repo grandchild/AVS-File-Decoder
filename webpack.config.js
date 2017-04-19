9249282d89406a1e103610490bc556cb310391fa
Jan T. Sott <jan@idleberg.com>
2017-04-19 18:00:46 +0200

    common-js module (cli only)

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/lib/app.js',
+  output: {
+    path: __dirname + '/assets',
+    filename: 'js/app.js',
+    publicPath: "/assets"
+  },
+  target: 'web',
+  // module: {
+  //   loaders: [
+  //   {
+  //     test: /.jsx?$/,
+  //     loader: 'babel-loader',
+  //     exclude: /node_modules/,
+  //     query: {
+  //       presets: ['es2015', 'react']
+  //     }
+  //   }
+  //   ]
+  // },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        to: './css/theme.css'
+      }
+    ])
+  ]
+};
