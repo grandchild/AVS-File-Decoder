8d46a4aa0457917d189c4e42967a50b430f1114c
Jan T. Sott <jan@idleberg.com>
2017-09-22 11:54:45 +0200

    add tests (not really!)

---
+++ test/default.js
@@ -0,0 +1,9 @@
+// Dependencies
+import { test } from 'ava';
+
+test('This is not the test you\'re looking for! 👻', t => {
+  const expected = 'true';
+  const actual = 'true';
+
+  t.is(actual, expected);
+});
