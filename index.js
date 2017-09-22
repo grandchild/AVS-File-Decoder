d137fc081e0e2bec5bee3e6092585cb5df5ad626
Jan T. Sott <jan@idleberg.com>
2017-09-22 11:46:02 +0200

    update comment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 /*  The TypeScript compiler does not support she-bangs,
- *  so we need this file :rolleyes:
+ *  so we need this work-around :rolleyes:
  */
 require('./build/cli.js');
