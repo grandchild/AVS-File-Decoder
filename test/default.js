f1a3a6bb5a2c47270877cbcd53d405b642bdb980
Jan T. Sott <jan@idleberg.com>
2017-09-22 12:17:32 +0200

    simplify test

--- test/default.js
+++ test/default.js
@@ -2,8 +2,5 @@
 import { test } from 'ava';
 
 test('These is not the tests you\'re looking for! 👻', t => {
-  const expected = 'true';
-  const actual = 'true';
-
-  t.is(actual, expected);
+  t.pass();
 });
