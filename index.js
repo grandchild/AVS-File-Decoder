c4e00999b51acc19384a9b44358d96e5d62e5e41
Jan T. Sott <jan@idleberg.com>
2017-11-01 16:00:33 +0100

    remove CLI parts

--- index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const join = require('path').join;
-
-/*  The TypeScript compiler does not support she-bangs,
- *  so we need this stupid workaround 🙄
- */
-const init = require(join(__dirname, 'bin/webvsc.js'));
