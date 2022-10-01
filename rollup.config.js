056f4edce2281b1d820e615fa5992cc416e2c87b
Jan T. Sott <git@idleberg.com>
2022-10-01 22:03:04 +0200

    refactor: export ESM only, use external tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -2,39 +2,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 
-const plugins = [commonjs(), json()];
-
-const compilerOptions = {
-    allowSyntheticDefaultImports: true,
-    typeRoots: ['./types', './node_modules/@types']
-};
-
+const plugins = [commonjs(), json(), typescript()];
 const external = ['chalk', 'fs', 'path'];
 
 export default [
-    // CommonJS
-    {
-        external,
-        input: 'src/browser.ts',
-        output: {
-            file: 'dist/browser.cjs',
-            format: 'cjs',
-            sourcemap: true
-        },
-        plugins: [...plugins, typescript(compilerOptions)],
-    },
-    {
-        external,
-        input: 'src/node.ts',
-        output: {
-            file: 'dist/node.cjs',
-            format: 'cjs',
-            sourcemap: true
-        },
-        plugins: [...plugins, typescript(compilerOptions)]
-    },
-
-    // ESM
     {
         external,
         input: 'src/browser.ts',
@@ -43,14 +14,7 @@ export default [
             format: 'esm',
             sourcemap: true
         },
-        plugins: [
-            ...plugins,
-            typescript({
-                ...compilerOptions,
-                module: 'es2020',
-                moduleResolution: 'node'
-            })
-        ]
+        plugins
     },
     {
         external,
@@ -60,13 +24,6 @@ export default [
             format: 'esm',
             sourcemap: true
         },
-        plugins: [
-            ...plugins,
-            typescript({
-                ...compilerOptions,
-                module: 'es2020',
-                moduleResolution: 'node'
-            })
-        ]
+        plugins
     }
 ];
