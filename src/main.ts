662b9485cfd8271385f8dc36d7f58b41c99cdf2d
Jan T. Sott <git@idleberg.com>
2023-01-06 01:48:50 +0100

    fix: allow throwing errors

--- src/main.ts
+++ src/main.ts
@@ -27,30 +27,17 @@ export function convertPreset(data: Buffer | ArrayBuffer, presetName: string, pr
     const preset = {
         name: presetName
     };
+
     if (presetDate) {
         preset['date'] = presetDate;
     }
+
     const blob8 = new Uint8Array(data);
-    try {
-        const clearFrame = decode.presetHeader(blob8.subarray(0, config.presetHeaderLength));
-        preset['clearFrame'] = clearFrame;
-        const components = Util.convertComponents(blob8.subarray(config.presetHeaderLength));
-        preset['components'] = components;
-    } catch (e) {
-        // TODO
-        // if (verbosity < 0) Log.error(`Error in '${file}'`);
-        if (verbosity >= 1) {
-            Log.error(e.stack);
-        } else {
-            Log.error(e);
-        }
-        // if(e instanceof Util.ConvertException) {
-        //     Log.error('Error: '+e.message);
-        //     return null;
-        // } else {
-        //     throw e;
-        // }
-    }
+    const clearFrame = decode.presetHeader(blob8.subarray(0, config.presetHeaderLength));
+    preset['clearFrame'] = clearFrame;
+
+    const components = Util.convertComponents(blob8.subarray(config.presetHeaderLength));
+    preset['components'] = components;
 
     return preset;
 }
