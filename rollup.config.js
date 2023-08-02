630fe77852d97b890cf4a94b763722ae41f5849d
Jan T. Sott <git@idleberg.com>
2023-08-02 07:38:52 +0200

    chore: switch back to rollup for type emitting

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    external: [
+        'json5',
+        'node:os'
+    ],
+    input: 'src/main.ts',
+    output: {
+        file: 'dist/webvsc.mjs',
+        format: 'esm',
+        sourcemap: true
+    },
+    plugins: [
+        typescript()
+    ]
+};
