248d9f2530010c74b74a20e47a43879fe6bf254a
Jan T. Sott <git@idleberg.com>
2023-11-05 23:55:09 +0100

    feature: add Preset type

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import * as Util from './lib/util';
 import config from './config';
 import decode from './lib/decode';
+import { Preset } from './schema/preset';
 
 // Constants
 let verbosity = 0; // log individual key:value fields
@@ -12,7 +13,7 @@ const defaultArgs: Webvsc.Arguments = {
 		verbose: 0
 };
 
-export function convertPreset(data: Buffer | ArrayBuffer, presetName: string, presetDate: string = undefined, customArgs: Webvsc.Arguments = {}): unknown {
+export function convertPreset(data: Buffer | ArrayBuffer, presetName: string, presetDate: string = undefined, customArgs: Webvsc.Arguments = {}): Preset {
 		const args = {
 				...defaultArgs,
 				...customArgs
@@ -23,20 +24,12 @@ export function convertPreset(data: Buffer | ArrayBuffer, presetName: string, pr
 		Util.setVerbosity(verbosity);
 		Util.setHiddenStrings(Boolean(args.hidden));
 
-		const preset = {
-				name: presetName
-		};
-
-		if (presetDate) {
-				preset['date'] = presetDate;
-		}
-
 		const blob8 = new Uint8Array(data);
-		const clearFrame = decode.presetHeader(blob8.subarray(0, config.presetHeaderLength));
-		preset['clearFrame'] = clearFrame;
 
-		const components = Util.convertComponents(blob8.subarray(config.presetHeaderLength));
-		preset['components'] = components;
-
-		return preset;
+		return {
+				name: presetName,
+				date: presetDate,
+				clearFrame: decode.presetHeader(blob8.subarray(0, config.presetHeaderLength)),
+				components: Util.convertComponents(blob8.subarray(config.presetHeaderLength))
+		} as Preset;
 }
---
+++ src/schema/preset.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+import { EffectList } from './effect-list';
+import { MiscEffects } from './misc';
+import { RenderEffects } from './render';
+import { TransEffects } from './trans';
+
+export type PresetComponents = EffectList | MiscEffects | RenderEffects | TransEffects;
+
+const presetBase = z.object({
+	name: z.string(),
+	date: z.string().datetime(),
+	clearFrame: z.boolean(),
+}).required();
+
+type PresetBase = z.infer<typeof presetBase>;
+export type Preset = PresetBase & {
+	components: PresetComponents[]
+};
